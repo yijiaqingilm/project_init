ba711ef2c031d4a68d5b79d9e48f526ce100614e
what <Administrator@2013-20160513BX>
2016-10-12 17:46:30 +0800

    update code

--- app/scripts/directives/my_directive.js
+++ app/scripts/directives/my_directive.js
@@ -11,6 +11,9 @@ app.directive("touchslide", function() {
 			autoPlay:'@autoplay'
 		},
 		link: function(scope, elem, attrs) {
+			console.log(scope);
+			console.log("touch silide ")
+			console.log(attrs);
 			TouchSlide({
 				slideCell: "#"+attrs.id,
 				titCell: ".hd ul", //开启自动分页 autoPage:true ，此时设置 titCell 为导航元素包裹层
